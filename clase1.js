a48d765bb983c55f204072e238ef006534311924
violafranco <franco5viola@gmail.com>
2022-01-17 08:36:12 -0300

    clase 1 y clase 2

---
+++ clase1.js
@@ -0,0 +1,33 @@
+/* -------------------------------------------------------------------------- */
+//Programa una función que cuente el número de caracteres de una cadena de texto
+let srt = 'Hola mundo, soy juan carlos';
+function contadorLetras(str) {
+    return str.length
+};
+//console.log(contadorLetras(srt));
+
+/* -------------------------------------------------------------------------- */
+// Programa una función que te devuelva el texto recortado según el número de caracteres indicados
+let texto = 'Hola mundo, soy juan carlos';
+function cortarTexto(str, nroCaracteres){
+    return str.slice(0, nroCaracteres)
+}
+//console.log(cortarTexto(texto, 6));
+
+/* -------------------------------------------------------------------------- */
+// Dado una String te devuelva un Array de textos separados por cierto caracter.
+// Ej: miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+let texto1 = 'Hola mundo, soy juan carlos';
+function separarTexto(str, tiposDeSeparacion){
+    return str.split(tiposDeSeparacion)
+}
+//Primero paso que quiero cortar y el segundo parametro es para decir que cuando eso aparezca, ahí meta un corte
+//console.log(separarTexto(texto1, ' '));
+
+/* -------------------------------------------------------------------------- */
+//Programa una función que repita un texto X veces.Ej: miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
+let texto2 = 'Hola juan carlos ';
+function repetir(str, nroVeces){
+    return str.repeat(nroVeces)
+}
+console.log(repetir(texto2, 2));
---
+++ clase2.js
@@ -0,0 +1,78 @@
+/* -------------------------------------------------------------------------- */
+//Programa una función que invierta las palabras de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".
+function reversa(str) {
+    return str.split("").reverse().join("");
+}
+//console.log(reversa('Hola Mundo'));
+/**/
+const invertirCadena = (str = '') => {
+    (!str) 
+    ? console.warn('Ingresa texto') 
+    : console.log(str.split('').reverse().join(''));
+}
+//invertirCadena('Hola Mundo');
+
+
+
+/* -------------------------------------------------------------------------- */
+// Programa una función para contar el número de veces que se repite una palabra en un texto
+// largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
+const contarRepeticiones = (str = '', palabra = '') => {
+    if(!str){return console.log('No Ingresaste un texto')};
+    if(!palabra) {return console.log('No ingresaste una palabra')};
+
+    let i = 0,
+     contador = 0;
+
+    while(i !== -1) {
+        i = str.indexOf(palabra, i)
+        if(i !== -1) {
+            i++;
+            contador++;
+        }
+    }
+    return console.info(`La palabra ${palabra} se repite ${contador} veces`);
+}
+//contarRepeticiones("hola mundo, adios mundo, hasta luego mundo", "mundo");
+
+
+
+
+/* -------------------------------------------------------------------------- */
+// Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee
+// igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
+function validarPalindromo(str){
+    if(str === str.split("").reverse().join("")){
+        return true
+    } else {
+        return false
+    }
+}
+//console.log(validarPalindromo('salas'));
+/**/
+const palindromo = (str = '') => {
+    if(!str) return console.warn('No ingresaste una palabra');
+
+    //convierto el texto a minúscula
+    str = str.toLowerCase();
+    let alReves = str.split("").reverse().join("");
+
+    return (str === alReves)
+    ? console.log('Si, la palabra es palíndromo')
+    : console.log('No, la palabra no es palíndromo');
+}
+//palindromo('salas')
+
+
+/* -------------------------------------------------------------------------- */
+// Programa una función que elimine cierto patrón de caracteres de un texto dado,
+// pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+const eliminarPatron = (str = '', patron = '') => {
+    (!str)
+    ? console.warn('No ingresaste un texto')
+    : (!patron) 
+        ? console.warn('No ingresaste un patrón')
+        : console.log(str.replace(new RegExp(patron, 'ig'), ''))
+}
+eliminarPatron('xyz1, xyz2, xyz3, xyz4 y xyz5', 'xyz')
+    
